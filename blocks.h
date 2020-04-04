ef138a9faf365a7899c4d3345a1b09b537ad5432
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2020-04-05 05:27:17 +0530

    added icons

--- blocks.h
+++ blocks.h
@@ -2,25 +2,25 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
-	{" ", "~/.local/bin/statusbar/music",5,12},
+	{"", "~/.local/bin/statusbar/music",5,12},
 
-	{" ", "~/.local/bin/statusbar/showRecord.sh",0,14},
+	{"", "~/.local/bin/statusbar/showRecord.sh",0,14},
 
-	{" ", "~/.local/bin/statusbar/caps.sh",0,15},
+	{"", "~/.local/bin/statusbar/caps.sh",0,15},
 
-	{" ", "~/.local/bin/statusbar/ip_addr.sh ethernet enp2s0f2",5,0},
+	{"", "~/.local/bin/statusbar/ip_addr.sh ethernet enp2s0f2",5,0},
 
-	{" ", "~/.local/bin/statusbar/ip_addr.sh wifi wlp3s0",5,0},
+	{"", "~/.local/bin/statusbar/ip_addr.sh wifi wlp3s0",5,0},
 
-	{" 📧 ", "~/.local/bin/statusbar/mailbox",0,13},
+	{"📬 ", "~/.local/bin/statusbar/mailbox",0,13},
 
-	{" ", "~/.local/bin/statusbar/volume",0,10},
+	{"", "~/.local/bin/statusbar/volume",0,10},
 
-	{" ☀ ", "xbacklight | sed 's/\\..*//'",	0,11},
+	{"☀: ", "~/.local/bin/statusbar/brightness",	0,11},
 	
-	{"  ", "~/.local/bin/statusbar/clock",	5,0},
+	{"📅 ", "~/.local/bin/statusbar/clock",	5,0},
 
-	{" ", "~/.local/bin/statusbar/battery BAT0",5,0},
+	{"", "~/.local/bin/statusbar/battery BAT0",5,0},
 
 };
 
