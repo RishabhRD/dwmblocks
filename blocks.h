3b60f270188d2d9b4e6be7cbc0b132274f8d18ca
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2020-04-05 00:15:49 +0530

    added my all shortcuts i think

--- blocks.h
+++ blocks.h
@@ -2,15 +2,25 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
-	{" ", "~/.local/bin/statusbar/music",0,5},
+	{" ", "~/.local/bin/statusbar/music",0,12},
+
+	{" ", "~/.local/bin/statusbar/showRecord.sh",0,14},
+
+	{" ", "~/.local/bin/statusbar/caps.sh",0,15},
+
+	{" ", "~/.local/bin/statusbar/ip_addr.sh ethernet enp2s0f2",5,0},
+
+	{" ", "~/.local/bin/statusbar/ip_addr.sh wifi wlp3s0",5,0},
+
+	{" 📧 ", "~/.local/bin/statusbar/mailbox",0,13},
 
 	{" ", "~/.local/bin/statusbar/volume",0,10},
 
 	{" ☀ ", "xbacklight | sed 's/\\..*//'",	0,11},
 	
-	{" ", "~/.local/bin/statusbar/battery BAT0",5,0},
-
 	{"  ", "~/.local/bin/statusbar/clock",	5,0},
+
+	{" ", "~/.local/bin/statusbar/battery BAT0",5,0},
 
 };
 
